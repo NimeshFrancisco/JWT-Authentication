64363fd616063ba8bf828723cc470c407e1974c7
Nimesh Francisco <nimesh7jfrancisco@gmail.com>
2022-08-24 16:31:02 +1000

    Project initiation

---
+++ JWTAuth/JWTAuth/Controllers/NameController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace JWTAuth.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NameController : ControllerBase
+    {
+        private readonly IJwtAuthenticationManager _jwtAuthenticationManager;
+        public NameController(IJwtAuthenticationManager jwtAuthenticationManager)
+        {
+            this._jwtAuthenticationManager = jwtAuthenticationManager;
+        }
+        // GET: api/<NameController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "naimesh", "jaith" };
+        }
+
+        // GET api/<NameController>/5
+        [HttpGet("{id}")]
+        public string Get(int id) 
+        {
+            return "value";
+        }
+        [AllowAnonymous]
+        [HttpPost("authenticate")]
+        public IActionResult Authenticate([FromBody] UserCred userCred)
+        {
+            var token=_jwtAuthenticationManager.Authenticate(userCred.Username, userCred.Password);
+            if (token==null)
+            {
+                return Unauthorized();
+            }
+            return Ok(token);
+        }
+
+        // POST api/<NameController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+    }
+
+}
---
+++ JWTAuth/JWTAuth/IJwtAuthenticationManager.cs
@@ -0,0 +1,7 @@
+﻿namespace JWTAuth
+{
+    public interface IJwtAuthenticationManager
+    {
+       string Authenticate(string username, string password);
+    }
+}
